6ee24542b1fb64e86d4fdd785271496b745d1493
EC2 Default User <ec2-user@ip-172-31-73-129.ec2.internal>
2022-09-06 11:41:25 +0000

    if

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,12 @@ class Reservation < ApplicationRecord
   validates :start_date, :end_date, :person, presence: true
   belongs_to :user
   belongs_to :room_registration
+  
+  def reservation
+    if @reservation.nil.valid?
+      'anonymous'
+    else
+      @reservation
+    end
+    
 end
